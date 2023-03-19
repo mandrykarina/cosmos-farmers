cc7f94cd1c88d41b8e463c5393edc5f228cdfbbb
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-03-19 11:45:36 +0300

    unity_tests

--- flask/tests.py
+++ flask/tests.py
@@ -1,9 +1,26 @@
-import flask.mainapp as mainapp
+import mainapp
 
-def check_resalt(result, *args):
-    if mainapp.energy(args) == result:
-        return True
-    return False
+def check_mass(result):
+    if mainapp.massa() == result:
+        return "The test is Completed!"
+    return "Test Failed!"
 
-print(check_resalt([696, 5800, 199], ))
+
+def check_energy(result, *args):
+    if mainapp.energy(*args) == result:
+        return "The test is Completed!"
+    return "Test Failed!"
+
+
+def check_connect():
+    if mainapp.get_new_flight_assigment():
+        return "The test is Completed!"
+    return "Test Failed!"
+
+# проверка функция массы, для стандартных значений вернет 
+print("\n")
+print(check_mass([696, 5800, 199]))
+print(check_energy(36, 8))
+print(check_connect())
+print("\n")
 
