f18734e257db654aa251824492dad6429dcca6ff
DimdonTM <74155113+DimdonTM@users.noreply.github.com>
2023-03-19 10:14:30 +0300

    Update MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -10,6 +10,14 @@ def get_new_flight_assigment(url="https://dt.miet.ru/ppo_it_final"):
     sait = get(url, headers={'X-Auth-Token': token})
     return dict(loads(sait.text))["message"]
 
+def speeds(V, w, m):
+    return V * (w/80) * (200/m)
+
+def energy(T):
+    t = 0
+    for i in range(0, T + 1):
+        t += i
+    return t
 
 
 
