35842dbce1b62bdd1dca28c55618c236dc8be70e
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-03-19 10:12:24 +0300

    new_block

--- MainApp.py
+++ MainApp.py
@@ -9,3 +9,11 @@ def get_new_flight_assigment(url="https://dt.miet.ru/ppo_it_final"):
     token = "46u76vrf"
     sait = get(url, headers={'X-Auth-Token': token})
     return dict(loads(sait.text))["message"]
+
+
+
+
+# <------------ ОСновной алгоритм программы -------------->
+
+flight_assigment = get_new_flight_assigment()
+
