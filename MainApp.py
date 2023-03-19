336a32d52851bf1eb4d8d9d6b14352feac249ba1
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-03-19 10:06:43 +0300

    function_for_get_requests_return_list_form_dictionary

--- MainApp.py
+++ MainApp.py
@@ -1,5 +1,11 @@
 from requests import get
+from json import loads
 
 
 
 # <------------ Обьявление функций -------------->
+
+def get_new_flight_assigment(url="https://dt.miet.ru/ppo_it_final"):
+    token = "46u76vrf"
+    sait = get(url, headers={'X-Auth-Token': token})
+    return dict(loads(sait.text))["message"]
